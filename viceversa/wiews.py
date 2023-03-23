19a2241ba0c41a967acd18ae230f73b48430bc29
PC <verbsmr@gmail.com>
2023-03-23 20:34:10 +0300

    Add words count feature

--- viceversa/wiews.py
+++ viceversa/wiews.py
@@ -4,5 +4,6 @@ def home(request):
 	return render(request,'home.html')
 def reverse(request):
 	get_user_text = request.GET['usertext']
+	count_words = len(get_user_text.split())
 	reversed_text = get_user_text[::-1]
-	return render(request,'reverse.html',{'usertext':get_user_text,'reversedtext':reversed_text})
+	return render(request,'reverse.html',{'usertext':get_user_text,'reversedtext':reversed_text,'countwords':count_words})
